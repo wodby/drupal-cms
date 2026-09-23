dc8c961680dec6aad36539c6d8bbbc39b2c926d9
Wodby Robot <robot@wodby.com>
2026-09-23 10:02:31 +0000

    Rebuild against updated base image digests

--- base-images.mk
+++ base-images.mk
@@ -3,7 +3,7 @@
 BASE_IMAGE_REPOSITORY := wodby/drupal-php
 BASE_IMAGE_VERSION_SUFFIX :=
 
-BASE_IMAGE_DIGEST_8.4 := sha256:eda15b3a8e31cb90d13aa0ffeea709accf9af54f2d70ca11a85d56a974df62af
+BASE_IMAGE_DIGEST_8.4 := sha256:c0f0cc633a8429520caf662acdea01c5f03da619ef33aba422435db717d9f9f8
 BASE_IMAGE_DIGEST_8.4-r1 := sha256:ffa1ae8d26f34b28f5b6c18bbedb369bcb86f7000b17263f9af00c6563e6484b
 
 # Fail before building when a version or variant has no reviewed pin.
